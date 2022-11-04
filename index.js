a6d7d31d48f917664f68fb80609e545463530de7
GeralmyUrena <geralmyuri11@gmail.com>
2022-11-04 19:09:38 -0400

    commit tree 5a2b4c85a8e81272acd0b5c80d6988f5fb0a29ab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(que = "roller-skate"){
+     return `This Saturday, I want to ${que}!`}
+
+const mondayWork = function (f = "go to the office") { return `This Monday, I will ${f}.`
+
+}
+
+
+//function saturdayFun (r = "roller-skate"){}
+
+
+function wrapAdjective(p="*"){return function(e="special"){return `You are ${p}${e}${p}!`}}
+
+////wrapAdjective("%")("You are a dedicated programmer!"); //=> "You are %a dedicated programmer%!"
+//wrapAdjective()("a hard worker!")
